1ab55b496a5123cca16ac11aaf5566210f2b4d4a
wirededdy <110313474+wirededdy@users.noreply.github.com>
2022-10-16 22:05:25 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<unistd.h>
 /**
- * main - Entry point
+ * main - prints the standrad error 
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
