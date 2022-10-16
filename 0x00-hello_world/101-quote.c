eee307e49bca35f6f59f9ac1f9d246570ed4fcbf
wirededdy <110313474+wirededdy@users.noreply.github.com>
2022-10-16 22:11:06 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
  * main - prints the standrad error
  *
- * Return: Always 1 (Success)
+ * Return: returns 1 (Sucess)
  */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
-}A
+}
