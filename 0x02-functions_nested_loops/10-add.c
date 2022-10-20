3b539c18482e5913dd1d462913d32866e20e64c8
wirededdy <110313474+wirededdy@users.noreply.github.com>
2022-10-20 04:41:28 +0000

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -prints add two numbers
+ * print int i
+ * print int k
+ * Return: Always 0.
+ */
+
+int add(int i, int k)
+{
+return (i + k);
+}
