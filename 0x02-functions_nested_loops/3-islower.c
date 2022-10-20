80abc570424de4eefee2b9466d1d3079f10ae75b
wirededdy <110313474+wirededdy@users.noreply.github.com>
2022-10-20 04:32:44 +0000

    compare me

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - Description
+ * it contains value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+
+{
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
